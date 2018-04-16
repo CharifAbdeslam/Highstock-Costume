5467d61450ddbdc8147e4d2d8812dd6c4a6fa615
unknown <charifabdeslem@gmail.com>
2018-04-16 10:13:56 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {compose} from 'redux';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
 import App from './components/App';
@@ -8,9 +9,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const store = createStore(reducers,
+compose(applyMiddleware(),
+         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+       ))
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
     <App />
     </Provider>,
     document.getElementById('root')

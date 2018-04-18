57ec9d043af754d423180cc7969b69ecdc84c429
unknown <charifabdeslem@gmail.com>
2018-04-18 15:39:50 +0100

    Update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,10 @@
-const DATA_URL = "https://api.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist?limit=1000";
+const DATA_URL = "https://api.bitfinex.com/v2/candles/trade:5m:tBTCUSD/hist?limit=1000";
 export const GET_DATA ='GET_DATA';
-
-const arrange = arr => {
-    return arr.map(([x,open,close,high,low,volume]) =>({x,open,close,high,low,volume}))     
-}
 
 export const getData = () => dispatch =>{
    fetch(DATA_URL).then(res => res.json())
-                   .then(data => dispatch({type:GET_DATA,payload:arrange(data)}))
+                  .then(data => dispatch({
+                    type:GET_DATA,
+                    payload:data.reverse()}
+                  ))
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,75 +1,90 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {getData} from '../actions/index'
+import {getData} from '../actions/index';
+import {Container} from 'reactstrap';
 import ReactHighstock from 'react-highcharts/ReactHighstock';
-
+require('highcharts/indicators/indicators')(Highcharts)
+require('highcharts/indicators/pivot-points')(Highcharts)
+require('highcharts/indicators/macd')(Highcharts)
+require('highcharts/modules/exporting')(Highcharts)
+require('highcharts/modules/map')(Highcharts)
 class App extends Component {
-
   componentWillMount() {
-       this.props.getData()
+    this.props.getData()
   }
-
+/*https://github.com/highcharts/highcharts-react/blob/master/package.json*/
   render() {
-    const config= {
+    const {data} = this.props;
+    var ohlc = [],
+      volume = [],
+      dataLen = data.length;
+    let i = 0;
+    for (i; i < dataLen; i += 1) {
+      ohlc.push([data[i][0], // the date
+        data[i][1], // open
+        data[i][3], // high
+        data[i][4], // low
+        data[i][2] // close
+      ]);
+      volume.push([data[i][0], // the date
+        data[i][5] // the volume
+      ]);
+    }
+    const config = {
       rangeSelector: {
-              buttons: [{
-                  type: 'hour',
-                  count: 1,
-                  text: '1h'
-              }],
-              selected: 0,
-              inputEnabled: false
-          },
-          plotOptions: {
- candlestick: {
-            color: 'red',
-            upColor: 'green'
-        }
-    },
-    yAxis: [{
-            labels: {
-                align: 'right',
-                x: -3
-            },
-            height: '80%',
-            lineWidth: 2,
-            resize: {
-                enabled: true
-            }
-        }, {
-            labels: {
-                align: 'right',
-                x: -3
-            },
-            top: '80%',
-            height: '20%',
-            offset: 0,
-            lineWidth: 2
-        }],
+        buttons: [
+          {
+            type: 'hour',
+            count: 1,
+            text: '1h'
+          }
+        ],
+        selected: 1,
+        inputEnabled: false
+      },
       title: {
-        text: 'BTC/USD 1mn'
+        text: 'AAPL Historical'
+      },
+      yAxis: [
+        {
+          height: '80%',
+          lineWidth: 2,
+          resize: {
+            enabled: false
+          }
+        }, {
+          top: '70%',
+          height: '20%',
+          offset: 0,
+          lineWidth: 2
+        }
+      ],
+
+      tooltip: {
+        split: true
       },
       chart:{
-        height:500
-      },
+             height:600
+     },
       series: [
         {
-          title:'price',
           type: 'candlestick',
-          id:'crypto',
-          data: this.props.data.reverse()
-        }]
-      }
+          name: 'AAPL',
+          data: ohlc,
+          id:'aapl'
+        }, {
+          type: 'column',
+          name: 'Volume',
+          data: volume,
+          yAxis: 1
+        }
+      ]
 
-    if(this.props.data == null){
-      return(<div>Loading ...</div>)
     }
-    return (<div className="container text-center">
+    return (<Container fluid={true}>
       <ReactHighstock config={config}/>
-    </div>);
+    </Container>);
   }
 }
-const mapStateToProps = state =>({
-  data : state.candle.all
-})
-export default connect(mapStateToProps,{getData})(App);
+const mapStateToProps = state => ({data: state.candle.all})
+export default connect(mapStateToProps, {getData})(App);
